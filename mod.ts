ba98c390d70b5e679f85cead3af8314dd11c21f1
迷渡 <justjavac@gmail.com>
2020-08-19 16:21:10 +0800

    release v0.1.0

--- mod.ts
+++ mod.ts
@@ -1,15 +1,44 @@
-/**
- * Every exported symbol ideally should have a documentation line.
- *
- * It is important that documentation is easily human readable,
- * but there is also a need to provide additional styling information to ensure
- * generated documentation is more rich text.
- * Therefore JSDoc should generally follow markdown markup to enrich the text.
- *
- * follow https://deno.land/std/style_guide.md
- *
- * @param foo - Description of non obvious parameter
- */
-export default function starter(foo: string): string {
-  return foo;
+const CTRLC = "\u0003";
+const CTRLD = "\u0004";
+const BACKSPACE = "\u007F";
+
+const decoder = new TextDecoder();
+const encoder = new TextEncoder();
+
+export default function getpass(prompt = "Password: "): string | null {
+  Deno.setRaw(Deno.stdin.rid, true);
+
+  Deno.stdout.writeSync(encoder.encode(prompt));
+
+  let w: string = "";
+
+  for (let thunk of Deno.iterSync(Deno.stdin)) {
+    const str = decoder.decode(thunk);
+    for (let i = 0; i < str.length; ++i) {
+      var ch = str[i];
+      switch (ch) {
+        case "\r":
+        case "\n":
+        case CTRLD:
+          cleanup();
+          return w;
+        case CTRLC:
+          cleanup();
+          throw new Error("Aborted");
+        case BACKSPACE:
+          w = w.slice(0, w.length - 1);
+          break;
+        default:
+          w += ch;
+          break;
+      }
+    }
+  }
+
+  return w;
+}
+
+function cleanup() {
+  Deno.stdout.writeSync(Uint8Array.of(13, 10)); // \r\n
+  Deno.setRaw(Deno.stdin.rid, false);
 }
